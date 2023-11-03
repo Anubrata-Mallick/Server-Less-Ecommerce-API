84a8a80acc4e59434db4acba3384ff99e70b1a6e
Anubrata Mallick <132219153+Anubrata-Mallick@users.noreply.github.com>
2023-11-03 09:18:05 +0530

    Create all.js

---
+++ orders/user/all.js
@@ -0,0 +1,41 @@
+// @see https://docs.aircode.io/guide/functions/
+const aircode = require('aircode');
+const {verifyToken} =require('../../helper/verifyToken');
+
+module.exports = async function (params, context) {
+  const tokenUser = await verifyToken(context);
+
+  if(tokenUser != null && tokenUser.isAdmin){
+    
+    const orderTable = aircode.db.table('order');
+  
+    try{
+      const orders = await orderTable
+      .where({"userId":tokenUser.userId})
+      .find()
+
+      const count = await orderTable
+      .where({"userId":tokenUser.userId})
+      .count()
+      
+      context.status(201);
+      return{
+        "count": count,
+        "orders":orders
+      }
+    }
+    catch(err){
+      context.status(402);
+      return{
+        "message":err.message
+      }
+    }
+  }
+  else{
+    context.status(401)
+    return{
+      "message":"User is not authorized"
+    }
+  }
+};
+
